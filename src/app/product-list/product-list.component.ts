c1fb7d82c046a958ef3796a07202b1dfc707eab3
Bibigul Otepbergenova <bibigul.tulegenovna@mail.ru>
2020-11-24 15:27:04 +0600

    making changes

--- src/app/product-list/product-list.component.ts
+++ src/app/product-list/product-list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import {ProductService} from '../services/product.service';
 
 @Component({
   selector: 'app-product-list',
@@ -7,9 +9,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductListComponent implements OnInit {
 
-  constructor() { }
-
+  constructor(private route: ActivatedRoute, private prodService: ProductService) { }
+Products: any;
   ngOnInit(): void {
+    if (this.route.snapshot.url.toString()){
+      // this.
+    }
+    this.prodService.getAllProduct().subscribe(
+      data => {
+        this.Products = data;
+        // console.log(data);
+      }
+    );
   }
-
 }
